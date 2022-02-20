fabb3be1901aa6f82bbc2c721a810d68461a01e3
Cristiano Salvatore Anselmi <cristiano.anselmi@gmail.com>
2022-02-20 19:38:11 +0100

    feat: add deploy script to s3

--- cart/src/constants.ts
+++ cart/src/constants.ts
@@ -1,4 +1,4 @@
 export enum hosts {
  localBuilder = 'http://localhost:3333/build/cart.esm.js',
- prodBuilder = 'http://localhost:3333/build/cart.esm.js',
+ prodBuilder = 'http://my-bucket-njoel24-replica.s3-website-us-east-1.amazonaws.com/cart/cart.esm.js',
 }
